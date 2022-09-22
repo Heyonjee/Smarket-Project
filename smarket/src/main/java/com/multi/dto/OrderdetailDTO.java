2dddd06f702e0ff4b3fea80603cbfa0d10179d2a
Heyonjee <guswl409@gmail.com>
2022-09-22 11:33:14 +0900

    20220922 hj

---
+++ smarket/src/main/java/com/multi/dto/OrderdetailDTO.java
@@ -0,0 +1,13 @@
+package com.multi.dto;
+
+public class OrderdetailDTO {
+	private int orderdetailno;
+	private int orderno;
+	private int productno;
+	private int ordersize;
+	private String orderitem;
+	private int price;
+	private int productamount;
+	
+
+}
---
+++ smarket/src/main/java/com/multi/dto/OrderlistDTO.java
@@ -0,0 +1,13 @@
+package com.multi.dto;
+
+import java.util.Date;
+
+public class OrderlistDTO {
+	private int orderno;
+	private String custid;
+	private int shipid;
+	private String orderstatus;
+	private Date orderdate;
+	private int totalprice;
+
+}
